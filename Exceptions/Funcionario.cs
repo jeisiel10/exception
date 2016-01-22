0f3a44c441debcf1382a5a138cc78ae7b11ae574
Jeisiel10 <jeisiel10@hotmail.com>
2016-01-22 10:36:20 -0200

    Exception

---
+++ Exceptions/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+	class Funcionario
+	{
+		private double salario;
+
+		public void AumentaSalario(double aumento)
+		{
+			if (aumento < 0)
+			{
+				ArgumentException erro = new ArgumentException();
+				throw erro;
+				
+			}
+
+		}
+	}
+}
---
+++ Exceptions/TestaFuncionario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+	class TestaFuncionario
+	{
+		static void Main(string[] args)
+		{
+			Funcionario f = new Funcionario();
+
+			try
+			{
+				f.AumentaSalario(-1000);
+			}
+			catch (ArgumentException erro)
+			{
+
+				Console.WriteLine("Houve uma ArgumentException ao aumentar o salário" );
+			}
+		}
+	}
+}
